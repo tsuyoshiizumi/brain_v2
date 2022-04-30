a5d643cbaf522e27fe53dbbc13101d10e4d85025
和泉剛 <>
2022-04-30 14:57:00 +0900

    usb2

--- usbdebug2.py
+++ usbdebug2.py
@@ -7,19 +7,8 @@ def port_p(location):
     ports = list(serial.tools.list_ports.comports())
     res = ''
     for p in ports:
-        if location == p.location:
-            print('kk')
-            print(p)
+        if p.location in location :
             print(p.device)
-            print(p.name)
-            print(p.description)
-            print(p.hwid)
-            print(p.vid)
-            print(p.pid)
-            print(p.serial_number)
             print(p.location)
-            print(p.manufacturer)
-            print(p.product)
-            print(p.interface)
-            res = p.name
+            res = p.device
     return res
